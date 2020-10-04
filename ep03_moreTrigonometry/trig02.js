9d7ce05b71a9e4af0a46712f4c44dc3bf8b94237
Chang Hyun Choi <taisetna@nate.com>
2020-10-04 17:00:10 +0900

    commit chapter 03

---
+++ ep03_moreTrigonometry/trig02.js
@@ -0,0 +1,33 @@
+window.onload = function() {
+    var canvas = document.getElementById("canvas"),
+    context = canvas.getContext("2d"),
+    width = canvas.width = window.innerWidth,
+    height = canvas.height = window.innerHeight;
+
+    var centerY = height * .5,
+        centerX = width  * .5,
+        baseRadius = 100,
+        baseAlpha = 0.5,
+        offset  = 50,
+        speed   = 0.1,
+        angle   = 0;
+    
+    render();
+
+    function render()
+    {
+        var radius = baseRadius + Math.sin(angle) * offset;
+        var alpha  = baseAlpha + Math.sin(angle) * offset;
+
+        context.fillStyle = "rgba(0,0,0," + alpha + ")";
+
+        context.clearRect(0,0, width, height);
+        context.beginPath();
+        context.arc(centerX, centerY, radius, 0, Math.PI * 2, false);
+        context.fill();
+
+        angle += speed;
+
+        requestAnimationFrame(render);
+    }
+}
